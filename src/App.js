9b19d680fc9b7cc42fdde3a75ca2f2080edad212
abeeral-rafati <abeerw.rafati@yahoo.com>
2021-09-01 04:13:52 +0300

    prouctepage-singin-signup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LandingPage from './components/Landing-page/LandingPage';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from "./components/Landing-page/LandingPage";
+import ProductPage from "./components/ProductPage/ProductPage";
+import SignIn from "./components/auth/SignIn/SignIn";
+import SignUp from "./components/auth/SingUp/SignUp";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Switch>
-          <Route exact path = '/'>
-            <LandingPage/>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route exact path="/product">
+            <ProductPage />
+          </Route>
+          <Route exact path="/signin">
+            <SignIn />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
           </Route>
         </Switch>
       </Router>
