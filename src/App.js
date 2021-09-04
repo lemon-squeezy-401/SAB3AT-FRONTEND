39d824ff7206df064b23d12cd285b5ebab7fe0e9
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-09-04 11:48:02 +0300

    done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import LandingPage from './components/Landing-page/LandingPage';
-import ProductPage from './components/ProductPage/ProductPage';
 import Login from './components/auth/Login/Login';
+import LandingPage from './components/Landing-page/LandingPage';
+import ProductCards from './components/ProductPage/ProductCards/ProductCards';
+import ProductDetails from './components/ProductPage/ProductDetails/ProductDetails';
+import ServiceCards from './components/ServicePage/ServiceCards/ServiceCards.js';
+import ServiceDetails from './components/ServicePage/ServiceDetails/ServiceDetails';
 
 function App() {
   return (
@@ -15,12 +18,26 @@ function App() {
           <Route exact path="/">
             <LandingPage />
           </Route>
-          <Route exact path="/product">
-            <ProductPage />
-          </Route>
+
           <Route exact path="/login">
             <Login />
           </Route>
+
+          <Route exact path="/products">
+            <ProductCards />
+          </Route>
+
+          <Route exact path="/ProductDetails">
+            <ProductDetails />
+          </Route>
+
+          <Route exact path="/services">
+            <ServiceCards />
+          </Route>
+          <Route exact path="/ServiceDetails">
+            <ServiceDetails />
+          </Route>
+
         </Switch>
         <Footer />
       </Router>
---
+++ src/components/ServicePage/ServiceCards/ServiceCards.js
@@ -0,0 +1,224 @@
+import React, { Component } from 'react';
+import './ServiceCards.css';
+import test from '../../../assets/test.jpg';
+export class ServicePage extends Component {
+    render() {
+        return (
+            <div>
+
+                <nav className="services1 card--services1">
+                    <a href={"/ServiceDetails"} className="service1 card1 serviceSection1 cardpic1">
+                        {/* <span className="card_label1">Translation </span> */}
+
+                        {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" /> */}
+                        <div id="container">
+
+                            <div className="product-details ">
+
+                                <h1>Translation</h1>
+                                <span class="hint-star star">
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                                </span>
+
+                                <p class="information">Have your document translated into more than 100 languages by professional translators..
+
+                                </p>
+
+
+
+                                <div class="control">
+
+                                    <button class="btn">
+                                        <span class="price">$250</span>
+                                        <span class="shopping-cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
+                                        <span class="buy">Show details</span>
+                                    </button>
+
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </a>
+
+
+
+
+
+                    <a href={"/"} className="service1 card1">
+                        {/* <span className="card_label1">Business</span> */}
+                        <div id="container">
+
+                            <div class="product-details">
+
+                                <h1>CHRISTMAS TREE</h1>
+                                <span class="hint-star star">
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                                </span>
+
+                                <p class="information">" Let's spread the joy , here is Christmas , the most awaited day of the year.Christmas Tree is what one need the most. Here is the correct tree which will enhance your Christmas.</p>
+
+
+
+                                <div class="control">
+
+                                    <button class="btn">
+                                        <span class="price">$250</span>
+                                        <span class="shopping-cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
+                                        <span class="buy">Show details</span>
+                                    </button>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+
+                    <a href={"/"} className="service1 card1">
+                        {/* <span className="card_label1">Design</span> */}
+                        <div id="container">
+
+                            <div class="product-details">
+
+                                <h1>CHRISTMAS TREE</h1>
+                                <span class="hint-star star">
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                                </span>
+
+                                <p class="information">" Let's spread the joy , here is Christmas , the most awaited day of the year.Christmas Tree is what one need the most. Here is the correct tree which will enhance your Christmas.</p>
+
+
+
+                                <div class="control">
+
+                                    <button class="btn">
+                                        <span class="price">$250</span>
+                                        <span class="shopping-cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
+                                        <span class="buy">Show details</span>
+                                    </button>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+
+                    <a href={"/"} className="service1 card1">
+                        {/* <span className="card_label1">Marketing</span> */}
+                        {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" /> */}
+                        <div id="container">
+                            <div class="product-details">
+                                <h1>CHRISTMAS TREE</h1>
+                                <span class="hint-star star">
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                                </span>
+
+                                <p class="information">" Let's spread the joy , here is Christmas , the most awaited day of the year.Christmas Tree is what one need the most. Here is the correct tree which will enhance your Christmas.</p>
+                                <div class="control">
+
+                                    <button class="btn">
+                                        <span class="price">$250</span>
+                                        <span class="shopping-cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
+                                        <span class="buy">Show details</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+
+                    <a href={"/"} className="service1 card1">
+                        {/* <span className="card_label1">Programming </span> */}
+                        <div id="container">
+
+                            <div class="product-details">
+
+                                <h1>CHRISTMAS TREE</h1>
+                                <span class="hint-star star">
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                                </span>
+
+                                <p class="information">" Let's spread the joy , here is Christmas , the most awaited day of the year.Christmas Tree is what one need the most. Here is the correct tree which will enhance your Christmas.</p>
+
+
+
+                                <div class="control">
+
+                                    <button class="btn">
+                                        <span class="price">$250</span>
+                                        <span class="shopping-cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
+                                        <span class="buy">Show details</span>
+                                    </button>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+
+
+                    <a href={"/"} className="service1 card1">
+                        {/* <span className="card_label1">Video Design </span> */}
+                        <div id="container">
+
+                            <div class="product-details">
+
+                                <h1>CHRISTMAS TREE</h1>
+                                <span class="hint-star star">
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                    <i class="fa fa-star-o" aria-hidden="true"></i>
+                                </span>
+
+                                <p class="information">" Let's spread the joy , here is Christmas , the most awaited day of the year.Christmas Tree is what one need the most. Here is the correct tree which will enhance your Christmas.</p>
+
+
+
+                                <div class="control">
+
+                                    <button class="btn">
+                                        <span class="price">$250</span>
+                                        <span class="shopping-cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
+                                        <span class="buy">Show details</span>
+                                    </button>
+
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </a>
+                </nav>
+
+
+            </div >
+        )
+    }
+}
+
+export default ServicePage
