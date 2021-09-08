8816ea0395acd9009fed20d043fa2851075b5264
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-09-08 10:09:43 +0300

    done

--- src/components/Landing-page/LandingPage.jsx
+++ src/components/Landing-page/LandingPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import VerticalSlider from '../Vertical-Slider/VerticalSlider';
 
+import ProductSlider from './ProductSlider/ProductSlider';
+import ServiceSlider from './ServiceSlider/ServiceSlider';
+
 function LandingPage() {
   return (
     <div>
-      <VerticalSlider/>
+      <VerticalSlider />
+      <ProductSlider />
+      <ServiceSlider />
     </div>
   );
 }
